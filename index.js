d610e09040926a8c9ebcc94d967c0888eeb80825
Zhenhua yu <zyu0628@gmail.com>
2020-10-10 23:40:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   return object;
 }
 function deleteFromObjectByKey(object, key) {
-  delete object[key];
-  return object;
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
 }
